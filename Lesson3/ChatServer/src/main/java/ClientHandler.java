03b860ef4bf638634a6b7a9be420ce3c61341aab
Evengarr <Ravasect@gmail.ru>
2021-02-20 22:37:08 +0300

    домашнее задание к 3 уроку

---
+++ Lesson3/ChatServer/src/main/java/ClientHandler.java
@@ -0,0 +1,105 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private String nickname;
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String msg = in.readUTF();
+                        if (msg.startsWith("/auth ")) {
+                            String[] tokens = msg.split("\\s");
+                            String nickname = server.getAuthService().getNickname(tokens[1], tokens[2]);
+                            if (nickname != null && !server.isNickBusy(nickname)) {
+                                sendMsg("/auth " + nickname);
+                                this.nickname = nickname;
+                                server.subscribe(this);
+                                break;
+                            }
+                        }
+                    }
+                    while (true) {
+                        String msg = in.readUTF();
+                        if (msg.startsWith("/")) {
+                            if (msg.equals("/end")) {
+                                sendMsg("/end");
+                                break;
+                            }
+                            if (msg.startsWith("/w ")) {
+                                String[] tokens = msg.split("\\s", 3);
+                                server.privateMsg(this, tokens[1], tokens[2]);
+                            }
+                            if (msg.startsWith("/changenick ")) {
+                                String newNickname = msg.split("\\s", 2)[1];
+                                if (newNickname.contains(" ")) {
+                                    sendMsg("Ник не может содержать пробелов");
+                                    continue;
+                                }
+                                if (server.getAuthService().changeNickname(this.nickname, newNickname)) {
+                                    this.nickname = newNickname;
+                                    sendMsg("/changenick " + nickname);
+                                    sendMsg("Ник был изменен");
+                                    server.broadcastClientsList();
+                                } else {
+                                    sendMsg("Ник уже занят");
+                                }
+                            }
+                        } else {
+                            server.broadcastMsg(this.nickname, msg);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    ClientHandler.this.disconnect();
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        server.unsubscribe(this);
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Lesson3/ChatServer/src/main/java/Server.java
@@ -0,0 +1,94 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Server {
+    private final Vector<ClientHandler> clients;
+    private final AuthService authService;
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public Server() {
+        clients = new Vector<>();
+
+        if (!Database.connect()) {
+            throw new RuntimeException("Невозможно подключиться к базе данных");
+        }
+
+        authService = new DatabaseAuthService();
+
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Сервер работает на порту " + serverSocket.getLocalPort() + "...");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new ClientHandler(this, socket);
+                System.out.println("Пользователь подключился");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            Database.disconnect();
+            System.out.println("Сервер отключен");
+        }
+    }
+
+    public void broadcastMsg(String senderNickname, String msg) {
+        for (ClientHandler client : clients) {
+            if (client.getNickname().equals(senderNickname)) {
+                client.sendMsg("Вы: " + msg);
+            }else {
+                client.sendMsg(senderNickname + ": " + msg);
+            }
+        }
+    }
+
+    public void privateMsg(ClientHandler sender, String receiverNickname, String msg) {
+        if (sender.getNickname().equals(receiverNickname)) {
+            sender.sendMsg("сообщение: " + msg);
+            return;
+        }
+        for (ClientHandler client : clients) {
+            if (client.getNickname().equals(receiverNickname)) {
+                client.sendMsg(sender.getNickname() + " написал вам" + ": " + msg);
+                sender.sendMsg("написать " + receiverNickname + ": " + msg);
+                return;
+            }
+        }
+        sender.sendMsg("Пользователь " + receiverNickname + " не найден");
+    }
+
+    public void subscribe(ClientHandler Client) {
+        clients.add(Client);
+        broadcastClientsList();
+    }
+
+    public void unsubscribe(ClientHandler Client) {
+        clients.remove(Client);
+        broadcastClientsList();
+    }
+
+    public boolean isNickBusy(String nickname) {
+        for (ClientHandler client : clients) {
+            if (client.getNickname().equals(nickname)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void broadcastClientsList() {
+        StringBuilder sb = new StringBuilder(15 * clients.size());
+        sb.append("/clients ");
+        for (ClientHandler client : clients) {
+            sb.append(client.getNickname()).append(" ");
+        }
+        sb.setLength(sb.length() - 1);
+        String out = sb.toString();
+        for (ClientHandler client : clients) {
+            client.sendMsg(out);
+        }
+    }
+}
